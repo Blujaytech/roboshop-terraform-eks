b0d5905318c58350f2dc4b8067ef4832fd0f364a
Blujaytech <blujaytechnlogies@gmail.com>
2026-06-18 16:28:41 +0530

    project infra code pushed

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-86s-hari-0601"
+    bucket = "remote-state-86s-hari-0602"
     key    = "Chandu.pem"
     region = "us-east-1"
     use_lockfile = true
