8e04aa8f11cfc44df638a5e3efed051fa623b6e6
Blujaytech <blujaytechnlogies@gmail.com>
2026-06-18 17:48:13 +0530

    project infra code pushed

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,11 +1,56 @@
-module "vpc" {
-  source = "git::https://github.com/Blujaytech/roboshop-terraform-eks.git//00-vpc"
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
 
-  vpc_cidr              = var.vpc_cidr
-  project_name          = var.project_name
-  environment           = var.environment
-  vpc_tags              = var.vpc_tags
-  public_subnet_cidrs   = var.public_subnet_cidrs
-  private_subnet_cidrs  = var.private_subnet_cidrs
-  database_subnet_cidrs = var.database_subnet_cidrs
+  tags = merge(
+    var.vpc_tags,
+    {
+      Name = "${var.project_name}-${var.environment}"
+    }
+  )
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = local.az_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public-${local.az_names[count.index]}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private-${local.az_names[count.index]}"
+  }
+}
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-database-${local.az_names[count.index]}"
+  }
 }
