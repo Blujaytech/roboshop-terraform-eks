54f06c4a8bd992de950caaffdcb0b1193c5fe7c3
Blujay Technologies <blujaytechnlogies@gmail.com>
2026-06-26 17:07:14 +0530

    Update outputs.tf

--- 00-vpc/outputs.tf
+++ 00-vpc/outputs.tf
@@ -1,15 +1,23 @@
 output "vpc_id" {
-    value = module.vpc.vpc_id
+  value = aws_vpc.main.id
 }
 
-output "public_subnet_ids" {
-    value = module.vpc.public_subnet_ids
+output "public_subnet_1" {
+  value = aws_subnet.public_1.id
 }
 
-output "private_subnet_ids" {
-    value = module.vpc.public_subnet_ids
+output "public_subnet_2" {
+  value = aws_subnet.public_2.id
 }
 
-output "database_subnet_ids" {
-    value = module.vpc.public_subnet_ids
+output "bastion_sg" {
+  value = aws_security_group.bastion_sg.id
+}
+
+output "web_sg" {
+  value = aws_security_group.web_sg.id
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.terraform_bucket.bucket
 }
