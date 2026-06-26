b0f4abbb4fbbc93961708d48f1ac7489a92bd8ff
Blujay Technologies <blujaytechnlogies@gmail.com>
2026-06-26 17:10:32 +0530

    Update variables.tf

--- 00-vpc/variables.tf
+++ 00-vpc/variables.tf
@@ -1,30 +1,27 @@
-variable "vpc_cidr" {
-    default = "10.0.0.0/16"
+variable "aws_region" {
+  default = "us-east-1"
 }
 
 variable "project_name" {
-    default = "roboshop"
+  default = "roboshop"
 }
 
-variable "environment"{
-    default = "dev"
+variable "environment" {
+  default = "dev"
 }
 
-variable "vpc_tags" {
-    default = {
-        Purpose = "Roboshop VPC"
-        DontDelete = "true"
-    }
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidrs" {
-    default= ["10.0.1.0/24","10.0.2.0/24"]
+variable "public_subnet_1_cidr" {
+  default = "10.0.1.0/24"
 }
 
-variable "private_subnet_cidrs" {
-    default= ["10.0.11.0/24","10.0.12.0/24"]
+variable "public_subnet_2_cidr" {
+  default = "10.0.2.0/24"
 }
 
-variable "database_subnet_cidrs" {
-    default= ["10.0.21.0/24","10.0.22.0/24"]
+variable "bucket_name" {
+  description = "Globally unique S3 bucket name"
 }
